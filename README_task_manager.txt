a0a99b56b8aa86571734106dcb2344f9a323e59b
Jason Kelderman <46725040+JakJak90@users.noreply.github.com>
2020-06-29 11:06:21 +0200

    Upload of files completed 18/06/2020
    Upload of project files for a basic task manager program, extended upon in the following capstone project.
    Planning started 16/06/2020
    Project completed 18/06/2020
    Time line indicated in README_task_manager.txt

---
+++ README_task_manager.txt
@@ -0,0 +1,105 @@
+Task : Note, task at hand to be extended upon within a later capstone project.
+
+Part 1:
+
+	Create a task manager program for a small business to keep track of assignments
+	given to each worker, the date issued and date due, task description, and an
+	indication if task is complete or not. Data stored/retrieved from file "tasks.txt".
+
+	Program must allow users to login with a matching username/password combination
+	that has been stored, and display an error message if username/password incorrect.
+	Combinations stored and retrieved from file "user.txt".
+
+	After login, menu of options must be displayed for user registration, add task,
+	view all tasks, view tasks assigned to user specifically and exit. Each option must
+	perform the function indicated by its name.
+
+Part 2:
+
+	Format the program to restrict menu options: only the username admin must be allowed
+	to register users.
+	Additionally, admin must have a new menu option to display statistics
+
+
+Planning:
+
+Functions:
+
+	login:
+	
+		have user enter a username, then enter a password.
+			check if username/password combination appears in "user.txt" file
+			if both appear in combination continue to menu
+			if one or both do not match, print error message
+	
+	register user:
+		
+		Only allow admin user to access this option
+			use an if statement to check if user is admin upon login
+			if result false, display error message if user attempts to add a new user
+	
+		prompt user for a new username, password and confirm password
+			check that username doesn't exist already, print error message if it does
+			check that password and confirmed password match, print error message if not
+			write username/password combination to file "user.txt"
+		
+	add task:
+	
+		prompt user for username to which the task is assigned
+			check that user exists within "user.txt" file
+			if not, display error message and prompt again
+		
+		prompt user for task title
+		
+		prompt user for task description
+		
+		prompt user for task due date
+		
+		Write task to file "tasks.txt", defaulting completed to "No" and date assigned to current date
+			import module datetime
+			use input today = datetime.date.today()
+				for formatting, use today.strftime(%x). output result will formatted to local version
+				otherwise, use today.strftime(%d %b %Y) to output as dd mmm yyyy
+	
+	view all tasks
+		
+		display each task in an easy to ready format
+	
+	view my tasks
+		
+		display only tasks assigned to logged in user in an easy to read format
+	
+	display statistics
+		
+		only allow admin user to see and access this option
+			use an if statement to check if user is admin upon login
+			if result false, display error message if user attempts to display statistics
+		
+		when selected, option displays total number of tasks and total number of users
+
+
+Timeline :
+
+Readme file planning 16-06-2020
+
+Menu created 17-06-2020
+
+Login function created 17-06-2020
+
+register user function created 17-06-2020
+
+username check function created 17-06-2020
+	did not form part of initial planning
+	function created to prevent duplication of code in login and register user functions
+
+task check function created 18-06-2020
+	did not form part of initial planning
+	function created to prevent duplicate code in view_tasks, view_my_tasks and display_statistics functions
+
+add task function created 18-06-2020
+
+view tasks function created 18-06-2020
+
+view my tasks function created 18-06-2020
+
+display statistics function created 18-06-2020
---
+++ task_manager.py
@@ -0,0 +1,355 @@
+﻿#Program for managing tasks assigned to users, which writes to and reads tasks and users
+#from the "tasks.txt" and "user.txt" files respectively
+
+#Import required module
+
+import datetime
+
+#Define required functions
+
+#Function to create dictionary of username/password combinations
+
+def username_check() :
+
+    #Declare local variables
+    
+    users = {}
+
+    #Access "user.txt" file in read mode
+    
+    with open("user.txt", "r") as f :
+
+        #Run through "user.txt" file to check for matching combination
+        
+        for line in f :
+
+            user_check = line.strip("\n")
+            
+            #Prevent range out of bound errors as caused by empty lines
+            
+            if user_check != "" :
+                user_check = user_check.split(", ")
+                users.update({user_check[0]: user_check[1]})
+
+    return users
+
+#Function to create a list of tasks
+
+def task_check() :
+
+    #Declare local variables
+    
+    tasks = []
+
+    #Access "tasks.txt" file in read mode
+    
+    with open("tasks.txt", "r") as f :
+
+        #Run through "tasks.txt" file to check for matching combination
+        
+        for line in f :
+
+            task_check = line.strip("\n")
+            
+            #Prevent range out of bound errors as caused by empty lines
+            
+            if task_check != "" :
+                task_check = [task_check.split(", ")]
+                tasks.extend(task_check)
+
+    return tasks
+            
+
+#Function to log onto task manager program by matching username/password combination
+
+def login() :  
+
+    #Get username and password combination from user
+        
+    username = input("Please enter your username:\n")
+    password = input("Please enter your password:\n")
+    print()
+
+    #Call username_check() function to compare login details to dictionary storage
+
+    user_list = username_check()
+    
+
+        
+    if username in user_list and user_list.get(username) == password :
+        return username
+
+    else :
+        print("Your username and/or password is incorrect, please ensure your caps lock is off and try again.\n")
+        return "loop"
+            
+#Function to display appropriate menu
+
+def menu() :
+
+    #Declare local variables
+
+    carry_on = True
+
+    #While loop to continue returning to menu until user selects exit
+
+    while carry_on :
+        print("Please select one of the following options:\n")
+        print("r\t- register user\n")
+        print("a\t- add task\n")
+        print("va\t- view all tasks\n")
+        print("vm\t- view my tasks\n")
+        
+        #Only show the display statistics option if admin is logged in
+        if admin_rights :
+            print("d\t- display statistics\n")
+        
+        print("e\t- exit")
+        print()
+        selection = input()
+        print()
+
+        #if-elif-else statement to call appropriate function based on menu selection
+        
+        if selection.lower() == "r" :
+            register_user()
+            
+        elif selection.lower() == "a" :
+            add_task()
+            
+        elif selection.lower() == "va" :
+            view_tasks()
+            
+        elif selection.lower() == "vm" :
+            view_my_tasks()
+            
+        elif selection.lower() == "d" :
+            display_statistics()
+            
+        elif selection.lower() == "e" :
+            carry_on = False
+            
+        else :
+            print("Your selection did not meet a menu item. Please try again.\n")
+
+#Function to register a new user to "user.txt" file
+            
+def register_user() :
+
+    #Declare local variables
+
+    carry_on = True
+
+    user_list = username_check()
+
+    #check that user has admin rights
+
+    if admin_rights == False :
+        print("You do not have permission to perform this action.\n")
+
+    else :
+
+        #While loop to ensure correct details are added
+
+        while (carry_on) :
+
+            #Access "user.txt" file in append mode
+        
+            with open("user.txt", "a") as f :
+
+                #Get username to be added from admin
+            
+                new_username = input("Please enter the username you would like to add, or 'e' to exit:\n")
+                print()
+
+                #check that username does not exist
+
+                if new_username in user_list :
+                    print("That username is already taken, please try a different username.\n")
+
+                else :
+                    password = input(f"Please enter a password for {new_username}:\n")
+                    print()
+                    confirmation = input("Please confirm the password entered:\n")
+
+                    if password == confirmation :
+                        f.write(f"\n{new_username}, {password}\n")
+                        print(f"new user {new_username} has been added!")
+                        print()
+                        carry_on = False
+
+                    else :
+                        print("Your password and confirmation do not match, please try again.\n")
+
+
+#Function to add a task to "tasks.txt" file
+
+def add_task() :
+
+    #Declare local variables
+
+    on_user_list = True
+
+    #Call user_check for validation
+
+    users_list = username_check()
+
+    #Access the "tasks.txt" file in append mode
+    
+    with open("tasks.txt", "a") as f :
+        
+        #Get task information from user
+        while (on_user_list) :
+            asigned_user = input("Who is the owner for this task?\n")
+
+        #Ensure that assigned user is in the "user.txt" file
+            if asigned_user in users_list :
+                on_user_list = False
+            else :
+                print("The given user does not exist, please try again.\n")
+            
+        title = input("What is the task title?\n")
+        description = input("Please enter a description of the task:\n")
+
+        #Determine assigned date from local time
+        
+        today = datetime.date.today()
+
+        #Format date to be in dd mmm yyyy format
+        
+        today = today.strftime("%d %b %Y")
+        
+        date_due = input("Please enter when the task is due in dd mmm yyyy format:\n")
+        complete = "No"
+
+        f.write(f"\n{asigned_user}, {title}, {description}, {today}, {date_due}, {complete}\n")
+        
+
+#Function to view all tasks within the "tasks.txt" file
+
+def view_tasks() :
+    
+    #Get list of registered tasks
+    
+    tasks = task_check()
+    
+    #Loop through each tasks to print them
+    
+    for i in range(0, len(tasks)) :
+        
+        #Break task down into assignable pieces
+        
+        asigned_user = tasks[i][0]
+        title = tasks[i][1]
+        description = tasks[i][2]
+        date_assigned = tasks[i][3]
+        date_due = tasks[i][4]
+        complete = tasks[i][5]
+        
+        #Print task in a user friendly manner
+        
+        print("-"*50)
+        print(f"Task:\t\t\t{title}")
+        print(f"Assigned to:\t\t{asigned_user}")
+        print(f"Date assigned:\t\t{date_assigned}")
+        print(f"Due date:\t\t{date_due}")
+        print(f"Task complete?\t\t{complete}")
+        print(f"Task description:\n{description}")
+    
+    print("-"*50)
+    print()
+            
+        
+
+#Function to view all tasks assigned to the logged in user within the "tasks.txt" file
+
+def view_my_tasks() :
+    
+    #Get list of registered tasks
+    
+    tasks = task_check()
+    
+    #Loop through each tasks to print them
+    
+    for i in range(0, len(tasks)) :
+        
+        #Break task down into assignable pieces
+        
+        asigned_user = tasks[i][0]
+        title = tasks[i][1]
+        description = tasks[i][2]
+        date_assigned = tasks[i][3]
+        date_due = tasks[i][4]
+        complete = tasks[i][5]
+        
+        #If user and assigned_user are the same, print task in a user friendly manner
+        if user == asigned_user :
+            print("-"*50)
+            print(f"Task:\t\t\t{title}")
+            print(f"Assigned to:\t\t{asigned_user}")
+            print(f"Date assigned:\t\t{date_assigned}")
+            print(f"Due date:\t\t{date_due}")
+            print(f"Task complete?\t\t{complete}")
+            print(f"Task description:\n{description}")
+    
+    print("-"*50)
+    print()
+
+#Function to display a count of all tasks and all users contained within "user.txt" and "tasks.txt" files
+
+def display_statistics() :
+    
+    #Declare local variables
+    
+    outstanding_tasks = 0
+
+    #Error message if none admin user enters "d" as a menu option
+    
+    if admin_rights == False :
+        print("Your selection did not meet a menu item. Please try again.\n")
+        return
+    
+    #Get count of users
+    
+    users = username_check()
+    user_count = len(users.keys())
+    
+    print(f"The total number of registered users is {user_count}.")
+    
+    #Get count of registered tasks 
+    
+    tasks = task_check()
+    task_count = len(tasks)
+    
+    #Check how many are still outstanding
+    
+    for i in range(0, task_count) :
+        if tasks[i][5].lower() == "no" :
+            outstanding_tasks += 1
+            
+    print(f"The total number of registered tasks is {task_count}, of which {outstanding_tasks} are still outstanding.")
+    print()
+
+#Declare gloabl variables
+
+login_attempt = True
+admin_rights = False
+
+#Main program
+
+#Loop to log onto task manager program
+#If admin logs on, assign admin rights
+
+while (login_attempt) :
+    user = login()
+
+    if user == "admin" :
+        admin_rights = True
+        login_attempt = False
+
+    elif user != "loop" :
+        login_attempt = False
+
+menu()
+
+
